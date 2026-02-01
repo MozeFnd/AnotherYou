cf232c32fc1468fa9c3386ca54796ceec9f2222e
XlxlICE <130644549+XlxlICE@users.noreply.github.com>
2026-02-01 11:26:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -294,4 +294,4 @@ def serve_image(filename):
     return send_from_directory('images', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=7860)
